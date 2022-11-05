6d4d2f4576321bc0fa661050568156516b98b66c
jungminna03 <96573337+jungminna03@users.noreply.github.com>
2022-11-05 23:17:01 +0900

    moving complet

--- Assets/Script/Controller/Car.cs
+++ Assets/Script/Controller/Car.cs
@@ -7,8 +7,19 @@ public class Car : MonoBehaviour
     Define.CarDir carDir;
     Define.CarType carType;
 
+    bool moving = true;
+
     private void Update()
     {
-        transform.Translate(Vector3.right * Time.deltaTime);
+        if(moving)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * 1.5f);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider != null)
+            moving = false;
     }
 }
--- Assets/Script/Scene/Stage1.cs
+++ Assets/Script/Scene/Stage1.cs
@@ -20,6 +20,8 @@ public class Stage1 : BaseScene
         GameManager.UI.ShowSceneUI<UI_Game>();
 
         BindSpawnPoint();
+
+        StartCoroutine(Spawn());
     }
 
     void BindSpawnPoint()
@@ -27,7 +29,7 @@ public class Stage1 : BaseScene
         spawnPoint = new Dictionary<int, Define.Lain>()
         {
             { 1, Define.Lain.First },
-            { 2, Define.Lain.None },
+            { 2, Define.Lain.Seceond },
             { 3, Define.Lain.None },
             { 4, Define.Lain.None },
             { 5, Define.Lain.None },
@@ -52,8 +54,45 @@ public class Stage1 : BaseScene
             { 24, Define.Lain.Seceond },
             { 25, Define.Lain.None },
             { 26, Define.Lain.None },
-            { 27, Define.Lain.All },
-            { 28, Define.Lain.None },
+            { 27, Define.Lain.First },
+            { 28, Define.Lain.Seceond },
+            { 29, Define.Lain.None },
+            { 30, Define.Lain.First },
+        };
+    }
+
+    void BindInformation()
+    {
+        spawnPoint = new Dictionary<int, Define.Lain>()
+        {
+            { 1, Define.Lain.First },
+            { 2, Define.Lain.Seceond },
+            { 3, Define.Lain.None },
+            { 4, Define.Lain.None },
+            { 5, Define.Lain.None },
+            { 6, Define.Lain.None },
+            { 7, Define.Lain.None },
+            { 8, Define.Lain.None },
+            { 9, Define.Lain.None },
+            { 10, Define.Lain.None },
+            { 11, Define.Lain.None },
+            { 12, Define.Lain.Seceond },
+            { 13, Define.Lain.None },
+            { 14, Define.Lain.None },
+            { 15, Define.Lain.None },
+            { 16, Define.Lain.None },
+            { 17, Define.Lain.None },
+            { 18, Define.Lain.First },
+            { 19, Define.Lain.First },
+            { 20, Define.Lain.None },
+            { 21, Define.Lain.None },
+            { 22, Define.Lain.None },
+            { 23, Define.Lain.None },
+            { 24, Define.Lain.Seceond },
+            { 25, Define.Lain.None },
+            { 26, Define.Lain.None },
+            { 27, Define.Lain.First },
+            { 28, Define.Lain.Seceond },
             { 29, Define.Lain.None },
             { 30, Define.Lain.First },
         };
@@ -61,23 +100,28 @@ public class Stage1 : BaseScene
 
     IEnumerator Spawn()
     {
-        for (int i = 0; i < spawnPoint.Count; i++)
+        for (int i = 1; i <= spawnPoint.Count; i++)
         {
-            GameObject go = GameManager.Resource.Instantiate("Object/Car");
+            Debug.Log(spawnPoint[i]);
             switch (spawnPoint[i])
             {
                 case Define.Lain.None:
                     break;
                 case Define.Lain.First:
-                    GameObject go = GameManager.Resource.Instantiate("Object/Car");
-                    go.transform.position = SpawnPoint1.position;
+                    {
+                        GameObject go = GameManager.Resource.Instantiate("Object/Car");
+                        go.transform.position = SpawnPoint1.position;
+                    }
                     break;
                 case Define.Lain.Seceond:
+                    {
+                        GameObject go = GameManager.Resource.Instantiate("Object/Car");
+                        go.transform.position = SpawnPoint2.position;
+                    }
                     break;
-                case Define.Lain.All:
-                    break;
-
             }
+
+            yield return new WaitForSeconds(1.5f);
         }
     }
 }
