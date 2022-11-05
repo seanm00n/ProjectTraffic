f02decbd3ea5f5d174bd9174713d1f9393e2367f
seanm00n <deccj97@naver.com>
2022-11-05 22:18:10 +0900

    GameController processing

--- Assets/Script/Controller/CarCtrl.cs
+++ Assets/Script/Controller/CarCtrl.cs
@@ -4,31 +4,5 @@ using Unity.PlasticSCM.Editor.WebApi;
 
 public class CarCtrl : MonoBehaviour
 {
-    public Transform tr; //오브젝트 transform
-    public Vector3 trPos = new Vector3(0, 0, 0); //오브젝트 position vector
-    public float moveCoolTime = 0f; //이동 거리 계산용 변수
-    public bool m_isArrived = false; //도착 확인용 변수
 
-    void Start()
-    {
-        tr = GetComponent<Transform>();
-    }
-    void Update()
-    {
-        CarMove(m_isArrived);
-    }
-    void CarMove(bool p_isArrived)
-    {
-        if (!p_isArrived)
-        {
-            moveCoolTime += Time.deltaTime;
-            trPos.x = (float)Math.Truncate(moveCoolTime);
-            tr.position = trPos;
-        }
-    }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        m_isArrived = true;
-        Debug.Log("Arriaved");
-    }
 }
--- Assets/Script/Controller/GameController01.cs
+++ Assets/Script/Controller/GameController01.cs
@@ -8,99 +8,34 @@ using UnityEngine;
 
 public class GameController01 : MonoBehaviour
 {
-    Destination destination = Destination.front; //차 방향
-    Species species = Species.Car; //차 종류
-
-    Car[] car; //객체 배열
-
     [SerializeField]
     int MAX_CAR_NUM;
-
+    CAR[] cars;
+    public struct CAR
+    {
+        public int number;
+        public int xPos;
+        public int genTime;
+        public Destination destination;
+        public Species species;
+        public GameObject obj;
+    }
     private void Start()
     {
-        car = new Car[MAX_CAR_NUM];
-        
-        Init();
-        StartCoroutine(Car1());
-        StartCoroutine(Car2());
-        StartCoroutine(Car3());
-        StartCoroutine(Car4());
-        StartCoroutine(Car5());
-        StartCoroutine(Car6());
-        StartCoroutine(Car7());
-        StartCoroutine(Car8());
-
+        cars = new CAR[MAX_CAR_NUM];
+        for (int i = 0; i < MAX_CAR_NUM; i++)
+        {
+            cars[i].number = i;
+            cars[i].xPos = i;
+            cars[i].species = Species.Car;
+            cars[i].obj = GameManager.Resource.Instantiate("Prefab/Car");
+        }
+        cars[0].destination = Destination.front;
+        cars[0].genTime = 1;
     }
     private void Update()
     {
-        
-    }
-    //게임 승리,패배 조건
-    void Init()
-    {
-        car[0].setDestination(Destination.front);
-        car[0].setPosition(0);
-        car[1].setDestination(Destination.up);
-        car[1].setPosition(1);
-        car[2].setDestination(Destination.front);
-        car[2].setPosition(0);
-        car[3].setDestination(Destination.down);
-        car[3].setPosition(0);
-        car[4].setDestination(Destination.up);
-        car[4].setPosition(1);
-        car[5].setDestination(Destination.up);
-        car[5].setPosition(0);
-        car[6].setDestination(Destination.front);
-        car[6].setPosition(1);
-        car[7].setDestination(Destination.down);
-        car[7].setPosition(0);
+         // = cars[0].xPos;
     }
 
-    IEnumerator Car1()
-    {
-        yield return new WaitForSeconds(1f);
-        car[0].Instanting();
-    }
-
-    IEnumerator Car2()
-    {
-        yield return new WaitForSeconds(12f);
-        car[1].Instanting();
-    }
-
-    IEnumerator Car3()
-    {
-        yield return new WaitForSeconds(18f);
-        car[2].Instanting();
-    }
-
-    IEnumerator Car4()
-    {
-        yield return new WaitForSeconds(19f);
-        car[3].Instanting();
-    }
-
-    IEnumerator Car5()
-    {
-        yield return new WaitForSeconds(24f);
-        car[4].Instanting();
-    }
-
-    IEnumerator Car6()
-    {
-        yield return new WaitForSeconds(27f);
-        car[5].Instanting();
-    }
-
-    IEnumerator Car7()
-    {
-        yield return new WaitForSeconds(27f);
-        car[6].Instanting();
-    }
-
-    IEnumerator Car8()
-    {
-        yield return new WaitForSeconds(30f);
-        car[7].Instanting();
-    }
 }
