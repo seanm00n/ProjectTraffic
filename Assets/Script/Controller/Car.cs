f11b017754afd77a2e380c4e7b5491bc4252abce
seanm00n <deccj97@naver.com>
2022-11-06 11:51:05 +0900

    add code
    feat CarCtrl

--- Assets/Script/Controller/Car.cs
+++ Assets/Script/Controller/Car.cs
@@ -1,19 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Timeline;
 using UnityEngine;
 
 public class Car : MonoBehaviour
 {
     Define.CarDir carDir;
     Define.CarType carType;
+    float speed;
 
     public bool moving = true;
 
     private void Update()
     {
+        speed += Time.deltaTime;
         if(moving)
         {
-            transform.Translate(Vector3.right * Time.deltaTime * 1.5f);
+            //transform.Translate(Vector3.right * Time.deltaTime * 1.5f);
+            transform.position = new Vector3
+                ((float)Math.Truncate(speed), transform.position.y, transform.position.z);
         }
     }
     private void OnCollisionEnter2D(Collision2D collision)
